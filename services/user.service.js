8eadb788a8da075faf8d121636445bac904bab67
Jonathan Guedes <jonathangueedes@gmail.com>
2020-06-30 00:31:55 -0300

    melhorado o tratamento dos dados.

--- services/user.service.js
+++ services/user.service.js
@@ -89,36 +89,67 @@ function create(userParam) {
     return deferred.promise;
 }
 
+//import { setLocale } from 'yup';
+
+// yup.setLocale({
+//     mixed: {
+//         default: 'Não é válido',
+//     },
+//     number: {
+//         min: '${path} deve ser maior que ${min}',
+//         max: '${path} deve ser maior que ${max}',
+//         positive: '${path} deve ser positivo',
+//         integer: '${path} deve ser um número inteiro',
+//         required: '${path} é um campo obrigatório'
+//     },
+
+//     string: {
+//         required: '${path} é um campo obrigatório'
+//     }
+// });
+
+let schema = yup.object().shape({
+    Name: yup.string().required(),
+    Username: yup.string().required(),
+    Idade: yup
+        .number()
+        .required("O Campo idade é obigatório")
+        .positive("O Valor deve ser positivo entre 12 a 100 anos de idade")
+        .integer("O campo deve ser um numero inteiro")
+        .min(12, "A idade minima aceitável é de 12 anos")
+        .max(100, "A idade maxima aceita é de 100 anos"),
+    Altura: yup
+        .number()
+        .required("O Campo altura é obigatório")
+        .positive("O Valor deve ser positivo entre 80 a 240 centimetros")
+        .min(80, "A altura minima aceitável é de 80 centimetros")
+        .max(240, "A altura maxima aceitavel é de 240 centimetros"),
+    Sexo: yup.string().required("Necessário definir um sexo"),
+    Peso: yup
+        .number()
+        .required("O campo peso é obrigatório")
+        .positive("O valor deve ser positivo entre 40 a 300 kilos")
+        .min(40, "O peso minimo aceitável é de 40 kilos")
+        .max(300, "O peso maximo aceitável é de 300 kilos"),
+    Endereco: yup.string()
+        .required("Campo endereço é de preenchimento obrigatório"),
+    Cep: yup.string()
+        .required('Campo obrigatório: Forneça um cep válido no formato 00000-000')
+        .matches(/^[0-9]{5}[-][0-9]{3}$/, 'Forneça um cep válido no formato 00000-000'),
+    Cidade: yup.string().required("Cidade tem que ser digitada"),
+    estado: yup.string().matches(/^(ac|AC|al|AL|am|AM|ap|AP|ba|BA|ce|CE|df|DF|es|ES|go|GO|ma|MA|mg|MG|ms|MS|mt|MT|pa|PA|pb|PB|pe|PE|pi|PI|pr|PR|rj|RJ|rn|RN|ro|RO|rr|RR|rs|RS|sc|SC|se|SE|sp|SP|to|TO)$/, "Digite uma UF válida"),
+    //objetivo: yup.string().required(),   
+    //dia_semana: yup.string().required(),
+    //periodo: yup.string().required(),
+    //info_complement: yup.string().required(),
+
+}).typeError("Erro geral");
+
+
 function update(_id, userParam) {
     var deferred = Q.defer();
 
-    let schema = yup.object().shape({
-        name: yup.string().required(),
-        username: yup.string().required(),
-        idade: yup
-            .number()
-            .required()
-            .positive()
-            .integer(),
-        altura: yup
-            .number()
-            .required()
-            .positive(),
-        sexo: yup.string().required(),
-        peso: yup
-            .number()
-            .required()
-            .positive(),
-        endereco: yup.string().required(),
-        cep: yup.string().required(),
-        cidade: yup.string().required(),
-        estado: yup.string().required(),
-        //objetivo: yup.string().required(),
-        //dia_semana: yup.string().required(),
-        //periodo: yup.string().required(),
-        //info_complement: yup.string().required(),
 
-    });
 
     // validation
     db.users.findById(_id, function(err, user) {
@@ -135,56 +166,48 @@ function update(_id, userParam) {
                         deferred.reject('Username "' + req.body.username + '" is already taken')
                     } else {
                         schema
-                            .isValid({
-                                name: userParam.name,
-                                username: userParam.username,
-                                idade: userParam.idade,
-                                altura: userParam.altura,
-                                sexo: userParam.sexo,
-                                peso: userParam.peso,
-                                endereco: userParam.endereco,
-                                cep: userParam.cep,
-                                cidade: userParam.cidade,
+                            .validate({
+                                Name: userParam.name,
+                                Username: userParam.username,
+                                Idade: userParam.idade,
+                                Altura: userParam.altura,
+                                Sexo: userParam.sexo,
+                                Peso: userParam.peso,
+                                Endereco: userParam.endereco,
+                                Cep: userParam.cep,
+                                Cidade: userParam.cidade,
                                 estado: userParam.estado,
-
                             })
-                            .then(function(valid) {
-                                valid; // => true
-                                dadosValidados = valid
-                                if (dadosValidados) {
-                                    updateUser();
-                                } else {
-                                    //return err
-                                    deferred.reject('Dados invalidos')
-                                }
-
+                            .then(function(valid, err) {
+                                updateUser();
+                            })
+                            .catch(function(err) {
+                                deferred.reject(err.message)
                             });
                     }
                 });
         } else {
             schema
-                .isValid({
-                    name: userParam.name,
-                    username: userParam.username,
-                    idade: userParam.idade,
-                    altura: userParam.altura,
-                    sexo: userParam.sexo,
-                    peso: userParam.peso,
-                    endereco: userParam.endereco,
-                    cep: userParam.cep,
-                    cidade: userParam.cidade,
+                .validate({
+                    Name: userParam.name,
+                    Username: userParam.username,
+                    Idade: userParam.idade,
+                    Altura: userParam.altura,
+                    Sexo: userParam.sexo,
+                    Peso: userParam.peso,
+                    Endereco: userParam.endereco,
+                    Cep: userParam.cep,
+                    Cidade: userParam.cidade,
                     estado: userParam.estado,
 
                 })
-                .then(function(valid) {
-                    valid; // => true
-                    dadosValidados = valid
-                    if (dadosValidados) {
-                        updateUser();
-                    } else {
-                        deferred.reject('Dados invalidos')
-                    }
-                });
+
+            .then(function(valid, err) {
+                updateUser();
+
+            }).catch(function(err) {
+                deferred.reject(err.message);
+            });;
         }
     });
 
@@ -227,7 +250,9 @@ function update(_id, userParam) {
 
             },
             info_complement: userParam.info_complement,
-            dataCadastro: ultimaAtualizacao.toLocaleDateString("pt-BR")
+            dataCadastro: ultimaAtualizacao.toLocaleDateString("pt-BR"),
+            latidude: userParam.latitude,
+            longitude: userParam.longitude
 
 
 
